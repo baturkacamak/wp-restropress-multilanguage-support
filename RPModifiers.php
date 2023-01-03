21c29e2907fa92aaaa16c3b0bf8c7bc3f27a82b1
baturkacamak <batisonline@gmail.com>
2023-01-03 10:43:44 +0100

    added code comments refactor code updated wordpress meta information

--- RPModifiers.php
+++ RPModifiers.php
@@ -1,8 +1,21 @@
 <?php
 
+/**
+ * RPModifiers class provides support for language translation in the Restropress plugin
+ *
+ * The class registers several filters and an action that modify how certain options are stored and retrieved
+ * in the WordPress options table in a way that allows for language translation of these options.
+ *
+ * @package Restropress Multilanguage Support
+ * @author  Batur Kacamak
+ */
 class RPModifiers
 {
-
+    /**
+     * Array of option keys to filter
+     *
+     * @var array
+     */
     private static $optionKeys = [
         'rpress_settings',
         'rpress_options',
@@ -22,11 +35,88 @@ class RPModifiers
         $this->register();
     }
 
-    private function isWPMLActive()
+    /**
+     * Retrieve an option value
+     *
+     * @param mixed $value Original option value
+     * @param string $option Option name
+     *
+     * @return mixed Option value
+     */
+    public function getOption($value, $option)
     {
-        return defined('ICL_LANGUAGE_CODE') && ICL_LANGUAGE_CODE;
+        $option_language_key = $this->getOptionKey($option);
+
+        if ($value_lang = get_option($option_language_key)) {
+            $value = $value_lang;
+        }
+
+        return $value;
     }
 
+    /**
+     * Update an option value
+     *
+     * @param mixed $value New option value
+     * @param mixed $old_value Old option value
+     * @param string $option Option name
+     *
+     * @return mixed New option value
+     */
+    public function updateOption($value, $old_value, $option)
+    {
+        $option_key = $this->getOptionKey($option);
+
+        update_option($option_key, $value);
+
+        return $value;
+    }
+
+    /**
+     * Set the `$GLOBALS['rpress_options']` global variable to the value of a translated version of the
+     * `rpress_settings` option, if it exists
+     */
+    public function setRestropressOptions()
+    {
+        if (isset($GLOBALS['rpress_options'])) {
+            $option_key = $this->getOptionKey('rpress_settings');
+            if ($language_options = get_option($option_key)) {
+                $GLOBALS['rpress_options'] = $language_options;
+            }
+        }
+    }
+
+    /**
+     * Register filters and action to modify how certain options are stored and retrieved in the options table
+     */
+    protected function register()
+    {
+        // multi language support
+        // for restropress
+        foreach (self::$optionKeys as $index => $option_key) {
+            add_filter("pre_update_option_{$option_key}", [$this, 'updateOption'], 20, 3);
+            add_filter("option_{$option_key}", [$this, 'getOption'], 10, 2);
+        }
+
+        add_action('init', [$this, 'setRestropressOptions'], 50);
+    }
+
+
+    /**
+     * Check if the WPML plugin is active
+     *
+     * @return bool True if the WPML plugin is active, false otherwise
+     */
+    private function isWPMLActive()
+    {
+        return is_plugin_active('sitepress-multilingual-cms/sitepress.php');
+    }
+
+    /**
+     * Get the current language code
+     *
+     * @return string Language code
+     */
     private function getLanguageCode()
     {
         // set languages
@@ -39,54 +129,17 @@ class RPModifiers
         return $lang;
     }
 
+    /**
+     * Get the option key for a translated option
+     *
+     * @param string $option Option name
+     *
+     * @return string Translated option key
+     */
     private function getOptionKey($option)
     {
         $language_code = $this->getLanguageCode();
 
         return "{$option}-{$language_code}";
-    }
-
-    public function getOption($value, $option)
-    {
-        $option_language_key = $this->getOptionKey($option);
-
-        if ($value_lang = get_option($option_language_key)) {
-            $value = $value_lang;
-        }
-
-        return $value;
-    }
-
-    public function updateOption($value, $old_value, $option)
-    {
-        $option_key = $this->getOptionKey($option);
-
-        update_option($option_key, $value);
-
-        return $value;
-    }
-
-    public function setRestropressOptions()
-    {
-        if (isset($GLOBALS['rpress_options'])) {
-            $option_key = $this->getOptionKey('rpress_settings');
-            if ($language_options = get_option($option_key)) {
-                $GLOBALS['rpress_options'] = $language_options;
-            }
-        }
-    }
-
-    protected function register()
-    {
-        // multi language support
-        // for restropress
-        {
-            foreach (self::$optionKeys as $index => $option_key) {
-                add_filter("pre_update_option_{$option_key}", [$this, 'updateOption'], 20, 3);
-                add_filter("option_{$option_key}", [$this, 'getOption'], 10, 2);
-            }
-        }
-
-        add_action('init', [$this, 'setRestropressOptions'], 50);
     }
 }
--- restropress-multilanguage-support.php
+++ restropress-multilanguage-support.php
@@ -1,12 +1,15 @@
 <?php
-/*
-Plugin Name:  Restropress Multilanguage Support
-Plugin URL: https://batur.info/
-Description: This plugin adds multi language settings support for Restropress
-Version: 1.0.0
-Author: Batur Kacamak
-Author URI: https://batur.info/
-*/
+/**
+ * Plugin Name: Restropress Multilanguage Support
+ * Plugin URI: https://github.com/baturkacamak/wp-restropress-multilanguage-support
+ * Description: This plugin adds multilanguage support to the Restropress plugin.
+ * Version: 1.0.1
+ * Author: Batur Kacamak
+ * Author URI: https://batur.info/
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
 require_once 'RPModifiers.php';
 
 new RPModifiers();
